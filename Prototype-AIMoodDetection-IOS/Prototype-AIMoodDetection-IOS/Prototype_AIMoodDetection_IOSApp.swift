5cdadb5a1d5a84d86c7338f3fdf59ee10051a353
Reno Muijsenberg <r.muijsenberg@student.fontys.nl>
2023-02-23 13:28:42 +0100

    First commit

---
+++ Prototype-AIMoodDetection-IOS/Prototype-AIMoodDetection-IOS/Prototype_AIMoodDetection_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prototype_AIMoodDetection_IOSApp.swift
+//  Prototype-AIMoodDetection-IOS
+//
+//  Created by Reno Muijsenberg on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Prototype_AIMoodDetection_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
